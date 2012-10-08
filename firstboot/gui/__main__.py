3fcfb82d244119d5f1599f077fc18b35291627b5
Martin Sivak <msivak@redhat.com>
2012-10-08 18:02:18 +0200

    Mock the data structure for Password spoke

--- firstboot/gui/__main__.py
+++ firstboot/gui/__main__.py
@@ -6,12 +6,27 @@ import os
 import gi.overrides
 for p in os.environ.get("ANACONDA_WIDGETS_OVERRIDES", "").split(":"):
     gi.overrides.__path__.insert(0, p)
-    
+
+# Too bad anaconda does not have modularized logging
+from pyanaconda import anaconda_log
+anaconda_log.init()
+
 class O(object):
     pass
 
 g = gui.FirstbootGraphicalUserInterface(None, None, None)
-data = O()
+
+class O(object):
+    def __init__(self, d):
+        self.__dict__ = d
+        
+data = O({
+    "rootpw": O({
+        "lock": False,
+        
+        })
+    })
+
 g.setup(data)
 g.run()
 
