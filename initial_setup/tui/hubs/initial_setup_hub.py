d2655b8cd7b5cd81a1ba2673d9a9d1d098b97d48
Vratislav Podzimek <vpodzime@redhat.com>
2013-11-28 10:52:51 +0100

    Adapt to changes in anaconda tui spoke categories (#1035462)
    The anaconda commit d4417b299d9732b2c7608f55384395ec4ce30172 renames the
    'password' category to the 'user' category. We need to adapt to that change
    otherwise User and Root password spokes are omitted in the text mode Initial
    Setup.

--- initial_setup/tui/hubs/initial_setup_hub.py
+++ initial_setup/tui/hubs/initial_setup_hub.py
@@ -10,7 +10,7 @@ _ = lambda x: gettext.ldgettext("initial-setup", x)
 N_ = lambda x: x
 
 class InitialSetupMainHub(TUIHub):
-    categories = ["password", "localization"]
+    categories = ["user", "localization"]
 
     prod_title = product.product_title()
     if prod_title:
