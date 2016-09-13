6853d1fe54b1756b498f38ff10c3deb5c3971ec0
Martin Kolman <mkolman@redhat.com>
2016-09-13 14:14:40 +0200

    Reboot the system is EULA is not accepted (#1365539)
    Make sure that the system is rebooted if EULA is not accepted in Initial Setup.
    
    Resolves: rhbz#1365539

--- initial_setup/gui/gui.py
+++ initial_setup/gui/gui.py
@@ -2,7 +2,7 @@ from pyanaconda.ui.gui import QuitDialog, GraphicalUserInterface
 from initial_setup.product import product_title, is_final
 from .hubs import InitialSetupMainHub
 import os
-from gi.repository import Gdk
+from gi.repository import Gdk, Gtk
 import gettext
 from di import inject, usesclassinject
 
@@ -14,6 +14,27 @@ class InitialSetupQuitDialog(QuitDialog):
     MESSAGE = N_("Are you sure you want to quit the configuration process?\n"
                  "You might end up with an unusable system if you do. Unless the "
                  "License agreement is accepted, the system will be rebooted.")
+
+    def run(self):
+        """Wrap the parent run method to change behavior of the Quit button.
+
+        If the Quit button is clicked the QuitDialog will have rc == 1.
+        Unfortunately (from the Initial Setup point of view) GraphicalUserInterface
+        calls sys.exit(0) if QuitDialog rc == 1 - unlike Anaconda Initial Setup
+        still has some housekeeping to do even if it is told to Quit.
+
+        So we don't propagate the rc value back to GraphicalUserInterface
+        but instead just quit the GTK mainloop. This continues the execution
+        of the Initial Setup run script, which can do the necessary housekeeping,
+        such as checking if the system should be rebooted if the EULA is not accepted.
+
+        Eventually a more clean solution could be to add a property to QuitDialog
+        that GraphicalUserInterface would check and quite the GTK main loop instead of
+        running sys.exit(0).
+        """
+        rc = super(InitialSetupQuitDialog, self).run()
+        if rc == 1:
+            Gtk.main_quit()
 
 @inject(Gdk, product_title = product_title, is_final = is_final)
 class InitialSetupGraphicalUserInterface(GraphicalUserInterface):
@@ -39,4 +60,3 @@ class InitialSetupGraphicalUserInterface(GraphicalUserInterface):
         "spokes": [(basemask + ".spokes.%s", os.path.join(basepath, "spokes"))],
         "categories": [(basemask + ".categories.%s", os.path.join(basepath, "categories"))],
         }
-
