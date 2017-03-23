884aeb566ecaf58f2b48a4679cafbebe6fc82431
Martin Kolman <mkolman@redhat.com>
2017-03-23 17:37:15 +0100

    Run GUI on the s390 if possible (#1378082)
    Run the Initial Setup GUI on the s390 if:
    - the DISPLAY environmental variable is set
    - either of initial-setup.service or initial-setup-graphical.service
      is enabled
    
    This is achieved by executing the run-initial-setup script, which has
    been extended to detect if $DISPLAY is set.
    
    We can also remove the code for disabling the Initial Setup services
    after successful run from the s390 startup scripts as the run-initial-setup
    script already handles that.
    
    Resolves: rhbz#1378082

--- scripts/s390/initial-setup.csh
+++ scripts/s390/initial-setup.csh
@@ -1,21 +1,19 @@
 # initial-setup.csh
 
-set IS_EXEC = /usr/libexec/initial-setup/initial-setup-text
+set IS_EXEC = /usr/libexec/initial-setup/run-initial-setup
 set IS_UNIT = initial-setup.service
 
 # the initial-setup-text.service is deprecated, use initial-setup.service instead
 set IS_UNIT_TEXT = initial-setup-text.service
 
+# the initial-setup-graphical.service is deprecated, use initial-setup.service instead
+set IS_UNIT_GRAPHICAL = initial-setup-graphical.service
+
 # check if the Initial Setup unit is enabled and the executable is available
 # - either the initial-setup.service or initial-setup-text.service need to be enabled
-if ( ( { systemctl -q is-enabled $IS_UNIT } || { systemctl -q is-enabled $IS_UNIT_TEXT } ) && -x $IS_EXEC ) then
+if ( ( { systemctl -q is-enabled $IS_UNIT } || { systemctl -q is-enabled $IS_UNIT_TEXT } || { systemctl -q is-enabled $IS_UNIT_GRAPHICAL } ) && -x $IS_EXEC ) then
     # check if we're not on 3270 terminal and root
     if (( `/sbin/consoletype` == "pty" ) && ( `/usr/bin/id -u` == 0 )) then
-        $IS_EXEC --no-stdout-log
-        if ( $? == 0 ) then
-            # everything apparently went well, disable all relevant Initial Setup units
-            systemctl -q is-enabled $IS_UNIT && systemctl -q disable $IS_UNIT
-            systemctl -q is-enabled $IS_UNIT_TEXT && systemctl -q disable $IS_UNIT_TEXT
-        endif
+        $IS_EXEC
     endif
 endif
--- scripts/s390/initial-setup.sh
+++ scripts/s390/initial-setup.sh
@@ -1,23 +1,23 @@
 # initial-setup.sh
 
-IS_EXEC=/usr/libexec/initial-setup/initial-setup-text
+IS_EXEC=/usr/libexec/initial-setup/run-initial-setup
 IS_UNIT=initial-setup.service
 
 # the initial-setup-text.service is deprecated, use initial-setup.service instead
-IS_UNIT_TEXT=initial-setup.service
+IS_UNIT_TEXT=initial-setup-text.service
+# the initial-setup-graphical.service is deprecated, use initial-setup.service instead
+IS_UNIT_GRAPHICAL=initial-setup-graphical.service
 
 IS_AVAILABLE=0
 # check if the Initial Setup unit is enabled and the executable is available
-# - either the initial-setup.service or initial-setup-text.service need to be enabled
-systemctl -q is-enabled $IS_UNIT || systemctl -q is-enabled $IS_UNIT_TEXT && [ -f $IS_EXEC ] && IS_AVAILABLE=1
+# - either of the initial-setup.service, initial-setup-text.service or initial-setup graphical
+# need to be enabled
+UNIT_ENABLED=0
+systemctl -q is-enabled $IS_UNIT || systemctl -q is-enabled $IS_UNIT_TEXT || systemctl -q is-enabled $IS_UNIT_GRAPHICAL && UNIT_ENABLED=1
+[ $UNIT_ENABLED -eq 1 ] && [ -f $IS_EXEC ] && IS_AVAILABLE=1
 if [ $IS_AVAILABLE -eq 1 ]; then
     # check if we're not on 3270 terminal and root
     if [ $(/sbin/consoletype) = "pty" ] && [ $EUID -eq 0 ]; then
-        $IS_EXEC --no-stdout-log
-        if [ $? == 0 ]; then
-            # everything apparently went well, disable all relevant Initial Setup units
-            systemctl -q is-enabled $IS_UNIT && systemctl -q disable $IS_UNIT
-            systemctl -q is-enabled $IS_UNIT_TEXT && systemctl -q disable $IS_UNIT_TEXT
-        fi
+        $IS_EXEC
     fi
 fi
